096599a932d0eae9d5e75c9504c80b03ddc1ab8d
Tomas Leon Patlayan <tomipatlayan@gmail.com>
2022-02-04 20:58:43 +0200

    adding onchange submit and form style

--- components/ProductForm.jsx
+++ components/ProductForm.jsx
@@ -8,36 +8,48 @@ const ProductForm = () => {
     price: 0,
   });
 
-  const handleChange = (e) => {
-    console.log("pep");
+  const handleChange = ({ target: { name, value } }) => {
+    setProduct({ ...product, [name]: value });
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const resutl = await axios.post("/api/products", {
-      name: "product 1",
-      description: "pep pdircut",
-      price: 10000,
-    });
-    console.log(resutl);
+    await axios.post("/api/products", product);
   };
 
   return (
-    <div className="">
-      <form onSubmit={handleSubmit}>
+    <div className="w-full max-w-xs">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      >
         <label htmlFor="name">Name:</label>
-        <input type="text" name="name" onChange={handleChange} />
+        <input
+          type="text"
+          name="name"
+          onChange={handleChange}
+          className="shadow border rounded py-2 px-3 text-gray-700"
+        />
 
         <label htmlFor="price">Price:</label>
-        <input type="text" name="price" id="price" onChange={handleChange} />
+        <input
+          type="text"
+          name="price"
+          id="price"
+          onChange={handleChange}
+          className="shadow border rounded py-2 px-3 text-gray-700"
+        />
 
         <label htmlFor="description">Description:</label>
         <textarea
           name="description"
           rows="2"
           onChange={handleChange}
+          className="shadow border rounded py-2 px-3 text-gray-700"
         ></textarea>
 
-        <button>Save Product</button>
+        <button className="bg-blue-500 hover:bg-blue-700 py-2 px-4 rounded focus:outline-none focus:shadow-outline font-bold text-white">
+          Save Product
+        </button>
       </form>
     </div>
   );
