92b27af505375a037d7d92d141ecc1608c6eaa77
Tomas Leon Patlayan <tomipatlayan@gmail.com>
2022-02-05 14:15:05 +0200

    Deleting Products

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -1,9 +1,26 @@
 import { pool } from "../../../config/db";
 
 export default async function handler(req, res) {
+  switch (req.method) {
+    case "GET":
+      return await getProdcutById(req, res);
+    case "DELETE":
+      return await deleteProduct(req, res);
+    default:
+      break;
+  }
+}
+
+const getProdcutById = async (req, res) => {
   const { id } = req.query;
   const [result] = await pool.query("SELECT * FROM product WHERE id = ?", [id]);
-  console.log(result[0]);
 
   return res.status(200).json(result[0]);
-}
+};
+
+const deleteProduct = async (req, res) => {
+  const { id } = req.query;
+  const result = await pool.query("DELETE  FROM product WHERE id = ?", [id]);
+
+  return res.status(204).json();
+};
--- pages/products/[id].jsx
+++ pages/products/[id].jsx
@@ -3,10 +3,21 @@ import React from "react";
 import Layout from "../../components/Layout";
 
 const ProductDetail = ({ product }) => {
-  console.log(product)
+  const handleDelete = async (id) => {
+    const res = await axios.delete(`/api/products/${id}`);
+    console.log(res);
+  };
   return (
     <Layout>
-      <h1>Product</h1>
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      <p>{product.price}</p>
+      <button
+        className="bg-red-500 px-3 py-2  text-white hover:bg-red-700"
+        onClick={() => handleDelete(product.id)}
+      >
+        Delete
+      </button>
     </Layout>
   );
 };
@@ -15,7 +26,7 @@ export const getServerSideProps = async (context) => {
   const { data: product } = await axios.get(
     `http://localhost:3000/api/products/${context.query.id}`
   );
- 
+
   return {
     props: { product },
   };
